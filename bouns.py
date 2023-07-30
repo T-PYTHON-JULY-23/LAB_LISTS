4517e5ce5b9438c3181f9065e398c80ea4d0d9a4
sarahnaghi <sara.naghi@gmail.com>
2023-07-30 12:30:25 +0300

    Lab lists done

---
+++ bouns.py
@@ -0,0 +1,21 @@
+movies = [
+    ("The Shawshank Redemption", 1994, [9, 10, 10, 9, 8, 9]),
+    ("The Godfather", 1972, [10, 9, 8, 10, 9, 7]),
+    ("Pulp Fiction", 1994, [9, 8, 7, 8, 6, 5]),
+    ("The Dark Knight", 2008, [10, 9, 9, 8, 9, 8]),
+    ("Schindler's List", 1993, [8, 9, 9, 7, 6, 8]),
+    ("The Room", 2003, [1, 2, 3, 4, 5, 1])
+]
+
+
+movie1 = movies[0][2]
+
+sum = 0
+for i in movie1:
+     print(i)
+     sum += i
+     average=round(sum/len(movie1),2)
+if average < 6.0:
+     del movie1
+
+print(f"Average rating: {average}")
