9dc2966069585033f1c2edc79c0781d17efb3ac5
Lenah11 <129691418+Lenah11@users.noreply.github.com>
2023-07-31 10:40:49 +0300

    Update Lab_list.py + bounse

--- Lab_list.py
+++ Lab_list.py
@@ -27,6 +27,32 @@ odd_number_list = [number for number in range(1200,2000,125) if number%2!=0]
 print(odd_number_list)
 
 
+# Bonus
+
+## Movie Ratings Analysis
+movies = [
+    ("The Shawshank Redemption", 1994, [9, 10, 10, 9, 8, 9]),
+    ("The Godfather", 1972, [10, 9, 8, 10, 9, 7]),
+    ("Pulp Fiction", 1994, [9, 8, 7, 8, 6, 5]),
+    ("The Dark Knight", 2008, [10, 9, 9, 8, 9, 8]),
+    ("Schindler's List", 1993, [8, 9, 9, 7, 6, 8]),
+    ("The Room", 2003, [1, 2, 3, 4, 5, 1])
+]
+
+
+#Calculates the average rating for each movie.
+
+
+def analysis_movie(movies: list):
+    count = 1
+    for title, year, rating in movies:
+        average = round(sum(rating) / len(rating),1)
+
+        if average > 6:
+            print(f"{title}({year}) average : {average} ")
+            count +=1
+
+analysis_movie(movies)
 
 
 
