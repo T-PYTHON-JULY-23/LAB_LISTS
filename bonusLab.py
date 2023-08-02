d0b3614ab3c5c867da9203f24f8f72e94918f74e
Maram-F <78721143+Maram-F@users.noreply.github.com>
2023-08-02 21:09:41 +0300

    Delete bonusLab.py

--- bonusLab.py
+++
@@ -1,8 +0,0 @@
-movies = {
-    ("The Shawshank Redemption", 1994),
-    ("The Godfather", 1972,)
-    ("Pulp Fiction", 1994, ),
-    ("The Dark Knight", 2008,),
-    ("Schindler's List", 1993, ),
-    ("The Room", 2003, )
-}
